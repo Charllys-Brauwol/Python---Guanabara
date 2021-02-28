3997ab52768b02329a07a780e3e83ecda0474bf5
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2021-02-28 17:07:54 -0300

    Aula 01

--- aula 01.py
+++ aula 01.py
@@ -1,6 +1,7 @@
-//Recebe o nome no input//
+#Variável recebe dados pelo input
 nome = input ('Qual é seu nome?')
 idade = input ('Qual sua idade?')
 peso = input ('Qual seu peso?')
 
+#Print imprime na tela as variáveis
 print (nome, idade, peso)
